14a49ec331df4144a3d26664f9ce8fc2146ea045
Fahad Mustafa <34606382+Fahad-M@users.noreply.github.com>
2019-01-05 23:41:34 +0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+clear
+echo "Neutron Setup..."
+echo "Installing Mingw Cross compiler"
+sudo apt-get install mingw-w64
+echo "Install libstdc++"
+sudo apt-get install libstdc++
+echo "Installing Wine"
+sudo apt-get install wine-stable
+echo "Done"
