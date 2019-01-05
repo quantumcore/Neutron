e3614b098672f6285f46983493988ba3c1982bf2
Fahad Mustafa <34606382+Fahad-M@users.noreply.github.com>
2019-01-06 02:54:42 +0500

    Create messageBox.cpp

---
+++ Samples/messageBox.cpp
@@ -0,0 +1,9 @@
+// Windows.h Demo.
+// This file is to be compiled on Linux
+#include <windows.h>
+
+int main()
+{
+    MessageBoxA(NULL, "Hello, World!", "Neutron", MB_ICONINFORMATION);
+    return 0;
+}
